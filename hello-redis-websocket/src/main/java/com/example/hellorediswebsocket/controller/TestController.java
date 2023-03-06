05270565de27faeb6099aa632c2d2bf6f7f0b40a
Lucky-Star-999 <theboost1305@gmail.com>
2023-03-06 22:18:04 +0700

    Add Redis for List

--- hello-redis-websocket/src/main/java/com/example/hellorediswebsocket/controller/TestController.java
+++ hello-redis-websocket/src/main/java/com/example/hellorediswebsocket/controller/TestController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/test")
 public class TestController {
@@ -16,5 +19,15 @@ public class TestController {
     public String helloRedis() {
         redisTemplate.opsForValue().set("likelion", "hello redis");
         return (String) redisTemplate.opsForValue().get("likelion");
+    }
+
+    @GetMapping("/redis/list")
+    public List<String> helloRedis2() {
+        List<String> redisList = new ArrayList<>();
+        redisList.add("Element 1");
+        redisList.add("Element 2");
+        redisList.add("Element 3");
+        redisTemplate.opsForList().rightPushAll("redisList", redisList);
+        return (List<String>) redisTemplate.opsForList().range("redisList", 0, 2);
     }
 }
